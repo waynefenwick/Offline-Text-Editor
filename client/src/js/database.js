80c0234e6f8ef06dadbecb1ed4dca2fcb6560a89
Wayne <waynefenwick@gmail.com>
2023-08-03 19:58:26 -0500

    Working on database.js and editor.js to retrieve data from the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,20 +13,28 @@ const initdb = async () =>
   });
 
 // Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
+export const putDb = async (contentArray) => {
   const db = await openDB('jate', 1);
   const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  await store.add(content);
+
+  await store.clear(); // Clear the existing content in the store
+  await store.add(contentArray, 1); // Save the content as an array of strings
   await tx.complete;
 };
+
 
 // Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   const db = await openDB('jate', 1);
   const tx = db.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  return store.getAll();
+
+  // Retrieve the content (an array of strings) from IndexedDB
+  const contentArray = await store.get(1);
+
+  return contentArray || []; // Return the array or an empty array if not found
 };
 
 initdb();
+console.log('Database initialized.');
--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -1,9 +1,13 @@
-// Import methods to save and get data from the indexedDB database in './database.js'
 import { getDb, putDb } from './database';
 import { header } from './header';
 
 export default class {
   constructor() {
+    // Initialize the editor
+    this.initEditor();
+  }
+
+  async initEditor() {
     const localData = localStorage.getItem('content');
 
     // check if CodeMirror is loaded
@@ -22,22 +26,28 @@ export default class {
       tabSize: 2,
     });
 
-    // When the editor is ready, set the value to whatever is stored in indexeddb.
-    // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
-    getDb().then((data) => {
+    try {
+      // Wait for the database initialization and content retrieval
+      const data = await getDb();
+      console.log('Data retrieved from IndexedDB:', data); // Check if data is being retrieved
       console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
-    });
+      this.editor.setValue(data.join('\n') || localData || header); // Join the array of lines with newlines
+    } catch (error) {
+      console.error('Error retrieving data from IndexedDB:', error);
+      this.editor.setValue(localData || header);
+    }
 
     this.editor.on('change', () => {
       localStorage.setItem('content', this.editor.getValue());
     });
 
-    // Save the content of the editor when the editor itself loses focus
-    this.editor.on('blur', () => {
-      console.log('The editor has lost focus');
-      putDb(localStorage.getItem('content'));
-    });
-    
+// Save the content of the editor when the editor itself loses focus
+this.editor.on('blur', async () => {
+  console.log('The editor has lost focus');
+  const content = this.editor.getValue();
+  const lines = content.split('\n').filter((line) => line.trim() !== ''); // Remove empty lines
+  await putDb(lines); // Save the content as an array of strings
+});
+
   }
 }
